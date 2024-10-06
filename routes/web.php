96a0ffd0b05e12281c242158d5a0173edeca55e1
Marcelo Santos <mjdos.2014@gmail.com>
2024-10-06 15:32:18 -0300

    Ajuste

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\{ExemploController,
     MuscleContractilityController};
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return view('index');
+})->name('index');
 
 Route::get('/about', function () {
     return view('about');
